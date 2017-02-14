0ab9a20232cc7ef9b41f4d127b7e2f0d05524c56
Justin Samuels <blizm87@gmail.com>
2017-02-14 02:36:45 -0800

    Completed all tasks up to average cost of starships

--- sample.rb
+++ sample.rb
@@ -2,9 +2,49 @@ require 'rest-client'
 require 'json'
 
 def get_names
-  url = "https://swapi.co/api/people/1"
+  url = "https://swapi.co/api/people"
   response = RestClient.get(url)
-  data = JSON.parse(response.body)
-  # puts data.keys
-  # data["results"].map { |character| character["name"] }
+  data = JSON.parse(response)
+  data["results"].map { |character| puts character["name"] }
 end
+
+def get_weight
+  url = "https://swapi.co/api/people"
+  response = RestClient.get(url)
+  data = JSON.parse(response)
+  all_weight = 0
+  data["results"].map { |character|
+    all_weight += character["mass"].to_i
+    }
+  puts "The cumulative weight of everyone is: " + all_weight.to_s
+end
+
+def get_starships
+  url = "https://swapi.co/api/starships"
+  response = RestClient.get(url)
+  data = JSON.parse(response)
+  data["results"].map { |ship|
+    puts ship["name"]
+  }
+end
+
+def get_starships_cost
+  url = "https://swapi.co/api/starships"
+  response = RestClient.get(url)
+  data = JSON.parse(response)
+  total_ship_cost = 0
+  data["results"].map { |ship|
+    ship_cost = ship["cost_in_credits"].to_i
+    total_ship_cost += ship_cost
+  }
+  puts "The sum total of all ships is: " + total_ship_cost.to_s
+  num_of_ships = data["results"].count
+  puts "I am the total number of ships: " + num_of_ships.to_s
+  avg_cost = total_ship_cost.to_f / num_of_ships.to_f
+  puts "The average cost of a ship is: " + avg_cost.to_s
+end
+
+get_names
+get_weight
+get_starships
+get_starships_cost
