6c0672337d4a298a4931104f42859f42c5957665
Justin Samuels <blizm87@gmail.com>
2017-02-14 04:22:47 -0800

    Completed lab with the bonus

--- sample.rb
+++ sample.rb
@@ -1,5 +1,6 @@
 require 'rest-client'
 require 'json'
+require 'csv'
 
 def get_names
   url = "https://swapi.co/api/people"
@@ -44,7 +45,61 @@ def get_starships_cost
   puts "The average cost of a ship is: " + avg_cost.to_s
 end
 
+def men_women
+  people_csv_file = File.read("people.csv")
+  data = CSV.parse(people_csv_file)
+  people_arr = []
+  keys = data.slice(0)
+  data.shift(1)
+  data.map { |myobj|
+    people_arr.push( Hash[ keys.zip(myobj) ] )
+  }
+  total_of_men = 0
+  total_of_women = 0
+  people_arr.map { |person|
+    if person["gender"] == "m"
+      total_of_men += 1
+    else
+      total_of_women += 1
+    end
+  }
+  puts "The total number of men is: " + total_of_men.to_s
+  puts "The total number of women is: " + total_of_women.to_s
+  puts "The total number of people is: " + people_arr.size.to_s
+end
+
+def born_after_2000
+  people_csv_file = File.read("people.csv")
+  data = CSV.parse(people_csv_file)
+  people_arr = []
+  keys = data.slice(0)
+  data.shift(1)
+  data.map { |myobj|
+    people_arr.push( Hash[ keys.zip(myobj) ] )
+  }
+  people_arr.map { |person|
+    if person["dob"].to_i >= 2000
+      full_name = person["given_name"].to_s + ' ' + person["surname"].to_s
+      puts full_name
+    end
+  }
+end
+
+def get_starwars
+  url = "https://swapi.co/api"
+  response = RestClient.get(url)
+  data = JSON.parse(response)
+  starwars_api = File.open("starwars_api.rb", "w"){ |file|
+    file.write(data)
+  }
+  result = File.read("starwars_api.rb")
+  puts result
+end
+
 get_names
 get_weight
 get_starships
 get_starships_cost
+men_women
+born_after_2000
+get_starwars
