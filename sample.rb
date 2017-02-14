4061ee3ffccf7afd33a955c2fea60ecf90849766
Justin Samuels <blizm87@gmail.com>
2017-02-13 16:38:30 -0800

    storing latest lecture before computer dies

--- sample.rb
+++ sample.rb
@@ -1,9 +1,10 @@
 require 'rest-client'
 require 'json'
 
-url = "https://swapi.co/api/people/1"
-response = RestClient.get(url)
-
-data = JSON.parse(response.body)
-
-puts data["results"].map { |character| character["name"] }
+def get_names
+  url = "https://swapi.co/api/people/1"
+  response = RestClient.get(url)
+  data = JSON.parse(response.body)
+  # puts data.keys
+  # data["results"].map { |character| character["name"] }
+end
